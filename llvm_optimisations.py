0e836ab548c7e08f102254f83d9a2bf74e76a2aa
Wilfred Hughes <me@wilfred.me.uk>
2015-08-29 13:12:08 +0100

    Benchmarking LLVM optimisations.

---
+++ llvm_optimisations.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+#!/usr/bin/env python
+
+from time import time
+import os
+
+
+def timed_run(command, runs=10):
+    """Run BF implementation command with program specified, and return a
+    list of timings.
+
+    """
+    timings = []
+    for _ in range(runs):
+        start_time = time()
+        os.system("{} > /dev/null".format(command))
+        elapsed = time() - start_time
+        timings.append(elapsed)
+
+    return timings
+
+
+def compile_bf(program, llvm_opt):
+    """Compare our test programs with and without LLVM optimisations. We
+    use peephole optimisation from bfc, but no speculative execution.
+
+    """
+    compiler = "/home/wilfred/projects/bfc/target/debug/bfc"
+    os.system("{} {} --opt=1 --llvm-opt={}".format(compiler, program, llvm_opt))
+
+
+def bench_program(program):
+    print("Program: {}".format(program))
+    
+    print("Without LLVM optimisations: ")
+    compile_bf("{}.bf".format(program), llvm_opt=0)
+    print(timed_run("./{}".format(program)))
+
+    print("With LLVM optimisations: ")
+    compile_bf("{}.bf".format(program), llvm_opt=3)
+    print(timed_run("./{}".format(program)))
+    print()
+
+if __name__ == '__main__':
+    bench_program("hello_world")
+    bench_program("bottles")
+    bench_program("squares")
+    bench_program("fibs")
